9c4d68f1fd4df4b3a136df418187a6dcec97cdc2
RuslanPerepelitsa <ruslan.perepelitsa07@gmail.com>
2021-10-14 17:21:58 +0300

    Solution of task 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String voice;
+
+
+    public Cat(String voice) {
+        this.voice = voice;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+}
